71e76e18f7c847cfc421608c17631442964cdbb7
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2020-04-05 16:37:51 -0300

    meta tags og type

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,44 @@
 module.exports = {
-	siteMetadata: {
-		title: `Douglas "Amarelo" Lopes`,
-		description: `Front-end developer`,
-		author: `@DouglasAmarelo`,
-	},
-	plugins: [
-		`gatsby-plugin-react-helmet`,
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `images`,
-				path: `${__dirname}/src/assets`,
-			},
-		},
-		`gatsby-transformer-sharp`,
-		`gatsby-plugin-sharp`,
-		{
-			resolve: `gatsby-plugin-manifest`,
-			options: {
-				name: `Douglas "Amarelo" Lopes`,
-				short_name: `Douglas "Amarelo" Lopes`,
-				start_url: `/`,
-				background_color: `#663399`,
-				theme_color: `#663399`,
-				display: `minimal-ui`,
-				icon: `src/assets/DouglasLopes.png`, // This path is relative to the root of the site.
-			},
-		},
-		// this (optional) plugin enables Progressive Web App + Offline functionality
-		// To learn more, visit: https://gatsby.app/offline
-		'gatsby-plugin-offline',
-		'gatsby-plugin-sass',
-		{
-			resolve: `gatsby-plugin-google-analytics`,
-			options: {
-				trackingId: "UA-83956840-1",
-				head: false,
-			},
-		}
-
-	],
-}
+  siteMetadata: {
+    title: `Douglas 'Amarelo' Lopes | Front-end developer`,
+    description: `Sou desenvolvimento front-end desde 2010. Já participei de grandes projetos para clientes como, Whirlpool Brasil e Whirlpool Latam (Brastemp, Consul, Compra Certa, KitchenAid e Whirlpool), Sony, Itaú, Honda, Microsoft, Ambev, B.blend, Amazon, Terra, Editora Globo, Banco do Brasil, Contém 1g, L'Occitane entre muitos outros.`,
+    author: `@DouglasAmarelo`,
+    image: `/images/douglasamarelo.com.png`,
+    twitterUsername: `@DouglasAmarelo`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Douglas "Amarelo" Lopes`,
+        short_name: `Douglas "Amarelo" Lopes`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/assets/DouglasLopes.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    'gatsby-plugin-offline',
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-83956840-1',
+        head: false,
+      },
+    },
+  ],
+};
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,59 +1,109 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StaticQuery, graphql } from 'gatsby';
 import Helmet from 'react-helmet';
 
 // import Header from './header'
 // import './layout.css'
-import '../styles/index.scss'
+import '../styles/index.scss';
 import Footer from './footer';
 
 const Layout = ({ children }) => (
-	<StaticQuery
-		query={graphql`
-			query SiteTitleQuery {
-				site {
-					siteMetadata {
-						title,
-						description
-					}
-				}
-			}
-		`}
-		render={data => (
-			<>
-				<Helmet
-					title={`${data.site.siteMetadata.title} | ${data.site.siteMetadata.description}`}
-					meta={[
-						{
-							name: 'description',
-							content: 'Front-end developer'
-						},
-						{
-							name: 'keywords',
-							content: 'Front-end, javaScript, css, html'
-						}
-					]}
-				>
-					<html lang="pt-br" />
-					<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/brands.css" integrity="sha384-BKw0P+CQz9xmby+uplDwp82Py8x1xtYPK3ORn/ZSoe6Dk3ETP59WCDnX+fI1XCKK" crossorigin="anonymous" />
-					<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/fontawesome.css" integrity="sha384-4aon80D8rXCGx9ayDt85LbyUHeMWd3UiBaWliBlJ53yzm9hqN21A+o1pqoyK04h+" crossorigin="anonymous" />
-					<meta name="google-site-verification" content="YD5y4cRKEvX2ZN4nL9pVmYXWNAK3TR0Z5341d1k9OEM" />
-				</Helmet>
-				{/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `}
+    render={(data) => (
+      <>
+        <Helmet
+          title={`${data.site.siteMetadata.title} | ${data.site.siteMetadata.description}`}
+          meta={[
+            {
+              name: 'description',
+              content: 'Front-end developer',
+            },
+            {
+              name: 'keywords',
+              content: 'Front-end, javaScript, css, html',
+            },
+          ]}
+        >
+          <html lang="pt-BR" />
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.7.0/css/brands.css"
+            integrity="sha384-BKw0P+CQz9xmby+uplDwp82Py8x1xtYPK3ORn/ZSoe6Dk3ETP59WCDnX+fI1XCKK"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.7.0/css/fontawesome.css"
+            integrity="sha384-4aon80D8rXCGx9ayDt85LbyUHeMWd3UiBaWliBlJ53yzm9hqN21A+o1pqoyK04h+"
+            crossorigin="anonymous"
+          />
+          <meta
+            name="google-site-verification"
+            content="YD5y4cRKEvX2ZN4nL9pVmYXWNAK3TR0Z5341d1k9OEM"
+          />
+          {/* Primary Meta Tags */}
+          <title>Douglas 'Amarelo' Lopes | Front-end developer</title>
+          <meta
+            name="title"
+            content="Douglas 'Amarelo' Lopes | Front-end developer"
+          />
+          <meta
+            name="description"
+            content="Sou desenvolvimento front-end desde 2010. Já participei de grandes projetos para clientes como, Whirlpool Brasil e Whirlpool Latam (Brastemp, Consul, Compra Certa, KitchenAid e Whirlpool), Sony, Itaú, Honda, Microsoft, Ambev, B.blend, Amazon, Terra, Editora Globo, Banco do Brasil, Contém 1g, L'Occitane entre muitos outros."
+          />
 
-				<div>
-					{children}
-				</div>
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://douglasamarelo.com/" />
+          <meta
+            property="og:title"
+            content="Douglas 'Amarelo' Lopes | Front-end developer"
+          />
+          <meta
+            property="og:description"
+            content="Sou desenvolvimento front-end desde 2010. Já participei de grandes projetos para clientes como, Whirlpool Brasil e Whirlpool Latam (Brastemp, Consul, Compra Certa, KitchenAid e Whirlpool), Sony, Itaú, Honda, Microsoft, Ambev, B.blend, Amazon, Terra, Editora Globo, Banco do Brasil, Contém 1g, L'Occitane entre muitos outros."
+          />
+          <meta property="og:image" content="/images/douglasamarelo.com.png" />
 
-				<Footer />
-			</>
-		)}
-	/>
-)
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://douglasamarelo.com/" />
+          <meta
+            property="twitter:title"
+            content="Douglas 'Amarelo' Lopes | Front-end developer"
+          />
+          <meta
+            property="twitter:description"
+            content="Sou desenvolvimento front-end desde 2010. Já participei de grandes projetos para clientes como, Whirlpool Brasil e Whirlpool Latam (Brastemp, Consul, Compra Certa, KitchenAid e Whirlpool), Sony, Itaú, Honda, Microsoft, Ambev, B.blend, Amazon, Terra, Editora Globo, Banco do Brasil, Contém 1g, L'Occitane entre muitos outros."
+          />
+          <meta
+            property="twitter:image"
+            content="/images/douglasamarelo.com.png"
+          />
+        </Helmet>
+        {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+
+        <div>{children}</div>
+
+        <Footer />
+      </>
+    )}
+  />
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
